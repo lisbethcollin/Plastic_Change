b9949ffec3797a903898f93b7c9aeecec87f20e7
Frederikke <frfl0001@stud.kea.dk>
2024-04-01 23:08:04 +0200

    små ting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    extend: {
+      // ...
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        roed: "#FF1054",
+        tekstblaa: "#ff784f",
+        morkblaa: "#153677",
+        blaa: "#007CF9",
+        lysblaa: "#EDF6FF",
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
